a7b3935730b04a7962e60da913689a80dc109ec9
lutiousXt54 <abhradeepdebnath@gmail.com>
2024-12-01 20:38:20 +0530

    some slight change in structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config.js';
 import router from './routes/main.routes.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -8,6 +9,8 @@ const app = express();
 const port = process.env.PORT||4500;
 
 // api url
+app.use(cors());
+app.use(express.json())
 app.use('/api/v1',router);
 
 //listener function
